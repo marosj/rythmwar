f2dce6bb33a8ac39b67cacd0874d2c4209b18dca
marosj <marosjgithub@gmail.com>
2015-04-03 19:18:57 +0200

    Read template from file

--- src/main/java/com/marosj/servlet/DisplayServlet.java
+++ src/main/java/com/marosj/servlet/DisplayServlet.java
@@ -9,9 +9,21 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Map;
 
 @WebServlet(urlPatterns = "/display")
 public class DisplayServlet extends HttpServlet {
+
+    @Override
+    public void init() throws ServletException {
+        super.init();
+        Map<String, Object> conf = new HashMap<String, Object>();
+        conf.put("rythm.engine.mode", "dev");
+        Rythm.init(conf);
+    }
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse response) throws ServletException, IOException {
@@ -20,7 +32,9 @@ public class DisplayServlet extends HttpServlet {
 
         PrintWriter out = response.getWriter();
 
-        out.println(Rythm.render("Hello @who!", "World"));
+        Path mainTemplate = Paths.get("c:/tmp/main.rtm");
+
+        out.println(Rythm.render(mainTemplate.toFile(), "World"));
 
         out.close();
     }
