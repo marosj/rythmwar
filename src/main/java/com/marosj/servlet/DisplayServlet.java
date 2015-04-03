39b235168c26ebcf2496f17ba44bbe09eb6d1179
marosj <marosjgithub@gmail.com>
2015-04-03 17:08:50 +0200

    Initial commit

---
+++ src/main/java/com/marosj/servlet/DisplayServlet.java
@@ -0,0 +1,25 @@
+package com.marosj.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(urlPatterns = "/display")
+public class DisplayServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        response.setCharacterEncoding("UTF-8");
+
+        PrintWriter out = response.getWriter();
+
+        out.println("test");
+
+        out.close();
+    }
+}
